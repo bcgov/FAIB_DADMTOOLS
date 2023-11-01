9972c581261a35981b30fc7cb683a4aeef44005a
mwfowler <38699663+mwfowler@users.noreply.github.com>
2023-11-01 16:10:17 -0700

    Update createOracleFDWpg.R
    Added in logic to not delete the server and user mappings if they exist already

--- R/createOracleFDWpg.R
+++ R/createOracleFDWpg.R
@@ -10,6 +10,24 @@
 #'
 #' @examples coming soon
 
+FDWServerExists <- function(srvName, pgConnList){
+  sql <- glue('select * from information_schema.foreign_servers f where foreign_server_name = \'{srvName}\'')
+  r <- getTableQueryPG(sql, pgConnList)
+  if(nrow(r)>0){
+    return(TRUE)
+  }
+  else{FALSE}
+}
+
+FDWUserMappingExists <- function(srvName, userName, pgConnList){
+  sql <- glue('select * from information_schema.user_mappings f where foreign_server_name = \'{srvName}\' and authorization_identifier = \'{userName}\'')
+  r <- getTableQueryPG(sql, pgConnList)
+  if(nrow(r)>0){
+    return(TRUE)
+  }
+  else{FALSE}
+}
+
 
 createOracleFDWpg <- function(dbserver, oraUser, oraPassword, oratable,connList){
 
@@ -21,6 +39,7 @@ createOracleFDWpg <- function(dbserver, oraUser, oraPassword, oratable,connList)
     oraSchema <- ''
     oraTblNameNoSchema <- fdwTblName
   }
+<<<<<<< Updated upstream
   sendSQLstatement( paste0('drop foreign table if exists load.', oraTblNameNoSchema,';'),connList)
   sendSQLstatement(  paste0('drop user mapping if exists for postgres server oradb;'),connList)
   sendSQLstatement(  paste0('drop server if exists oradb cascade;'),connList)
@@ -29,6 +48,17 @@ createOracleFDWpg <- function(dbserver, oraUser, oraPassword, oratable,connList)
   sendSQLstatement(  paste0('create schema if not exists load;'),connList)
   sendSQLstatement(  paste0("create server oradb foreign data wrapper oracle_fdw options (dbserver '", dbserver, "' );"),connList)
   sendSQLstatement(  paste0("create user mapping for postgres server oradb options (user '", oraUser, "', password '", oraPassword,"');"),connList)
+=======
+  sendSQLstatement(glue('drop foreign table if exists {fdwSchema}.{oraTblNameNoSchema};'),connList)
+  sendSQLstatement(glue('create schema if not exists {fdwSchema};'),connList)
+
+  if(!(FDWServerExists('oradb', connList))){
+    sendSQLstatement(paste0("create server oradb foreign data wrapper oracle_fdw options (dbserver '", dbserver, "' );"),connList)
+  }
+  if(!(FDWUserMappingExists('oradb', 'postgres', connList))){
+    sendSQLstatement(  paste0("create user mapping for postgres server oradb options (user '", oraUser, "', password '", oraPassword,"');"),connList)
+  }
+>>>>>>> Stashed changes
 
   sendSQLstatement(  paste0('import foreign schema "', oraSchema,'" limit to (', oratable, ') FROM SERVER oradb INTO load;'),connList)
   print(paste0('created fdw for ', oratable ))
