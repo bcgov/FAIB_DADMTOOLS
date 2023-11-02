28e3d924016b1ca9e8b176fb1c1ac3a9739ac585
mwfowler <38699663+mwfowler@users.noreply.github.com>
2023-11-02 15:24:42 -0700

    Updated FDW Load

--- R/createOracleFDWpg.R
+++ R/createOracleFDWpg.R
@@ -39,16 +39,7 @@ createOracleFDWpg <- function(dbserver, oraUser, oraPassword, oratable,connList)
     oraSchema <- ''
     oraTblNameNoSchema <- fdwTblName
   }
-<<<<<<< Updated upstream
-  sendSQLstatement( paste0('drop foreign table if exists load.', oraTblNameNoSchema,';'),connList)
-  sendSQLstatement(  paste0('drop user mapping if exists for postgres server oradb;'),connList)
-  sendSQLstatement(  paste0('drop server if exists oradb cascade;'),connList)
-  sendSQLstatement(  paste0('drop schema if exists load cascade;'),connList)
 
-  sendSQLstatement(  paste0('create schema if not exists load;'),connList)
-  sendSQLstatement(  paste0("create server oradb foreign data wrapper oracle_fdw options (dbserver '", dbserver, "' );"),connList)
-  sendSQLstatement(  paste0("create user mapping for postgres server oradb options (user '", oraUser, "', password '", oraPassword,"');"),connList)
-=======
   sendSQLstatement(glue('drop foreign table if exists {fdwSchema}.{oraTblNameNoSchema};'),connList)
   sendSQLstatement(glue('create schema if not exists {fdwSchema};'),connList)
 
@@ -58,7 +49,6 @@ createOracleFDWpg <- function(dbserver, oraUser, oraPassword, oratable,connList)
   if(!(FDWUserMappingExists('oradb', 'postgres', connList))){
     sendSQLstatement(  paste0("create user mapping for postgres server oradb options (user '", oraUser, "', password '", oraPassword,"');"),connList)
   }
->>>>>>> Stashed changes
 
   sendSQLstatement(  paste0('import foreign schema "', oraSchema,'" limit to (', oratable, ') FROM SERVER oradb INTO load;'),connList)
   print(paste0('created fdw for ', oratable ))
