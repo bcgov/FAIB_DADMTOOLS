f4214dd5307df8404520b22c92687d751679d07c
Hailey Eckstrand <Hailey.Eckstrand@gov.bc.ca>
2024-03-15 13:39:57 -0700

    Add in fid as primary key, added comment, tidied up variable cleaning and added formatting

--- R/writeNoSpaTbl2PG.R
+++ R/writeNoSpaTbl2PG.R
@@ -7,7 +7,7 @@
 #' @param pk primary of table e.g. objectid
 #' @param select fields to keep in table e.g. "objectid,feature_id,segmentName"
 #' @param where_clause sql where clause e.g. segment = 'highway2'
-#' @param schema default database schema
+#' @param outSchema default database schema
 #' @param useGdal Set to TRUE if GDAL is installed on machine e.g. TRUE
 #'
 #' @return no return
@@ -15,64 +15,51 @@
 #'
 #' @examples coming soon
 
-writeNoSpaTbl2PG <- function(src, outTblName, connList, lyr = NULL, pk = NULL, select =NULL, where = NULL, schema = NULL) {
+writeNoSpaTbl2PG <- function(src, outTblName, connList, lyr = NULL, pk = NULL, select = NULL, where = NULL, outSchema = NULL, table_comment = NULL) {
 
   dbname <- connList["dbname"][[1]]
   user <- connList["user"][[1]]
-  dest <- glue::glue("PG:dbname={single_quote(dbname)} user={single_quote(user)}")
 
-  if(is.null(lyr)) {
+  # ## Tidy up lyr input
+  if(is_blank(lyr)) {
     lyr <- NULL
-  } else if(is.na(lyr) || lyr == ''){
-    lyr = NULL
   }
 
-  if(is.null(pk)) {
+  # ## Tidy up pk input
+  if(is_blank(pk)) {
     pk <- NULL
-  } else if(is.na(pk) || pk == ''){
-    pk = NULL
   }
 
-  if(is.null(select) || is.na(select) || select == '' ){
-    print('keep all attributes')
+  # ## Tidy up select
+  if(is_blank(select)) {
     select <- ''
   } else {
       select <- gsub(",shape", "", select)
       select <- gsub(",geometry", "", select)
+      select <- gsub(", shape", "", select)
+      select <- gsub(", geometry", "", select)
       if (endsWith(select, ',')) {
         select <-  substr(select, 1, nchar(select) - 1)
       }
-      ############ else{select <-  sprintf('"%s"', select)}
+      select <- paste('-select', select)
   }
 
-  print(select)
 
-  if(is.null(schema) || is.na(schema) || schema == '') {
-    outName <- outTblName
-    outName2 <- outTblName
-  } else {
-      outName <- paste0(schema, ".", outTblName)
-      outName2 <- glue("{schema}.{outTblName}")
-    }
+  if(is_blank(outSchema)) {
+    outTblName <- outTblName
+  }
 
-  if( is.null(where) ||is.na(where) || where == ''){
+  if(is_blank(where)) {
     where <- ''
   } else {
     where <- glue(glue::double_quote(where))
-  }
-  #
-  print('Creating non-spatial table')
-  print(outName)
-
-  if( is.null(schema) || is.na(schema) || schema == ''){
-    schema <- "SCHEMA=public"
-  } else {
-    schema <- glue("SCHEMA={schema}")
+    where <- glue("-where {where}")
   }
 
-  print(src)
-  print(dest)
-  print(lyr)
+
+  if(is_blank(outSchema)) {
+    outSchema <- "public"
+  }
 
   if (endsWith(src, '.shp')) {
     precision <- "-lco precision=NO"
@@ -80,53 +67,38 @@ writeNoSpaTbl2PG <- function(src, outTblName, connList, lyr = NULL, pk = NULL, s
     precision <- ''
   }
 
-
-  select <- if(select == '') {
-    select <- ''
-  } else {
-    select <- paste('-select', select)
-  }
-  where <- if(where == '') {
-    where <- ''
-  } else {
-    where <- paste('-where', where)
-  }
   print(paste('ogr2ogr',
               '-nlt NONE',
               '-overwrite',
               '-gt 200000',
               where,
               select,
-              paste('-nln', outName),
-              paste('-lco', schema),
+              paste('-nln',outTblName),
+              paste0('-lco SCHEMA=', outSchema),
               paste('-lco', 'OVERWRITE=YES'),
               '--config PG_USE_COPY YES',
               precision,
-              paste0('-f PostgreSQL PG:dbname=', dbname),
+              paste0('-f PostgreSQL PG:dbname=',dbname),
               src,
               lyr))
+
   print((system2('ogr2ogr',args=c('-nlt NONE',
                                   '-overwrite',
                                   '-gt 200000',
                                   where,
                                   select,
-                                  paste('-nln', outName),
-                                  paste('-lco', schema),
+                                  paste('-nln',outTblName),
+                                  paste0('-lco SCHEMA=', outSchema),
                                   paste('-lco', 'OVERWRITE=YES'),
                                   '--config PG_USE_COPY YES',
                                   precision,
-                                  paste0('-f PostgreSQL PG:dbname=', dbname),
+                                  paste0('-f PostgreSQL PG:dbname=',dbname),
                                   src,
                                   lyr), stderr = TRUE)))
 
-
-  #Create index on pk
-  if( !is.null(pk)) {
-    print('Creating index for non spatial table')
-    outTblNameNoPer <-  gsub("\\.", "_", outTblName)
-    faibDataManagement::sendSQLstatement(paste0("drop index if exists ", outTblNameNoPer,"_grskey_inx;"), connList)
-    print('Dropped index')
-    faibDataManagement::sendSQLstatement(paste0("create index ", outTblNameNoPer,"_grskey_inx",  " on ", outName2, "(", pk,");"), connList)
-    faibDataManagement::sendSQLstatement(glue("ANALYZE {outName2};"), connList)
-    }
+  # ## add harded coded fid primary key sequenctial integer
+  faibDataManagement::sendSQLstatement(glue("ALTER TABLE {outSchema}.{outTblName} DROP CONSTRAINT {outTblName}_pkey;"), connList)
+  faibDataManagement::sendSQLstatement(glue("ALTER TABLE {outSchema}.{outTblName} ADD COLUMN fid int GENERATED BY DEFAULT AS IDENTITY PRIMARY KEY;"), connList)
+  faibDataManagement::sendSQLstatement(table_comment, connList)
+  faibDataManagement::sendSQLstatement(glue("ANALYZE {outSchema}.{outTblName};"),connList)
   }
