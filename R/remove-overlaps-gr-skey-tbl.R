51ae17616834c46986c321bde9b8bc3bc8fd8914
Mcdougall <Iaian.Mcdougall@gov.bc.ca>
2025-02-19 13:44:13 -0800

    documentation fix in remove overlaps function

--- R/remove-overlaps-gr-skey-tbl.R
+++ R/remove-overlaps-gr-skey-tbl.R
@@ -1,17 +1,14 @@
-#' Update FAIB hectares database from input csv of datasets
+#' Creates new dataset gr_skey table with gr_skey key overlaps removed by either grouping values into arrays or keeping only rows with the max or min value in select field
 #'
-#' @param in_csv File path to data sources csv, Defaults to "config_parameters.csv"
-#' @param pg_conn_param Keyring object of Postgres credentials, defaults to dadmtools::get_pg_conn_list()
-#' @param ora_conn_param Keyring object of Oracle credentials, defaults to dadmtools::get_ora_conn_list()
-#' @param crop_extent list of c(ymin, ymax, xmin, xmax) in EPSG:3005, defaults to c(273287.5,1870587.5,367787.5,1735787.5)
-#' @param gr_skey_tbl Schema and table name of the pre-existing gr_skey table. Argument to be used with suffix and rslt_ind within in_csv. Defaults to "whse.all_bc_gr_skey"
-#' @param raster_schema If import_rast_to_pg = TRUE, schema of imported raster, defaults to "raster"
-#' @param template_tif The file path to the gr_skey geotiff to be used as a template raster, fefaults to "S:\\FOR\\VIC\\HTS\\ANA\\workarea\\PROVINCIAL\\bc_01ha_gr_skey.tif"
-#' @param mask_tif The file path to the geotiff to be used as a mask, defaults to "S:\\FOR\\VIC\\HTS\\ANA\\workarea\\PROVINCIAL\\BC_Boundary_Terrestrial.tif"
-#' @param data_src_tbl Schema and table name of the metadata table in postgres that updates with any newly imported layer. Defaults to "whse.data_sources"
-#' @param out_tif_path Directory where output tif if exported and where vector is temporally stored prior to import
-#' @param import_rast_to_pg If TRUE, raster is imported into database in raster_schema. Defaults to FALSE
-#'
+#' @param gr_skey_table Schema and table name of the pre-existing gr_skey table
+#' @param attribute_table the name of the attribute table containing the key (e.g. pgid) to join to gr_skey table. Attributes from this table will be in the final resultant table. If the table name is left blank, gr_skey table attributes will be used in the final resultant table.
+#' @param array_ind TRUE of FALSE, if True then output table will have fields grouped into arrays (by gr_skey)
+#' @param array_fields Vector of field names to be goruped as array
+#' @param remove_by_rank_ind True or False.  If True only gr_skey rows that have the largest/Smallest rank_field values will be kept
+#' @param rank_field the field used to rank as either largest or smallest when grouped by gr_skey
+#' @param rank_type ASC or DESC, when ASC then the rows with smallest rank field values will be kept. When DESC then rows with largest rank field values will be kept.
+#' @param pg_conn_param Defaults to get_pg_conn_list()
+
 #'
 #' @return no return
 #' @export
@@ -22,8 +19,8 @@ remove_overlaps_gr_skey_tbl <- function(gr_skey_table,
                                        attribute_table,
                                        array_ind     = TRUE,
                                        array_fields = NULL,
-                                       remove_by_sort_ind   = FALSE,
-                                       sort_field       = NULL,
+                                       remove_by_rank_ind   = FALSE,
+                                       rank_field       = NULL,
                                        rank_type    = "ASC",
                                        pg_conn_param  = dadmtools::get_pg_conn_list())
 {
@@ -58,8 +55,8 @@ remove_overlaps_gr_skey_tbl <- function(gr_skey_table,
 
 
   ####VERIFIY FIELDS are legitimate
-  if ((array_ind && remove_by_sort_ind) || (!array_ind && !remove_by_sort_ind)  ) {
-      print(glue("ERROR: array_ind and remove_by_sort_ind cannot both be TRUE or cannot both be FALSE"))
+  if ((array_ind && remove_by_rank_ind) || (!array_ind && !remove_by_rank_ind)  ) {
+      print(glue("ERROR: array_ind and remove_by_rank_ind cannot both be TRUE or cannot both be FALSE"))
       stop()
   }
 
@@ -90,31 +87,31 @@ remove_overlaps_gr_skey_tbl <- function(gr_skey_table,
     dadmtools::run_sql_r(glue("ALTER TABLE {gr_skey_table}_duplicates_as_array ADD PRIMARY KEY (gr_skey);"), pg_conn_param)
 
 
-    }else if (remove_by_sort_ind){
+    }else if (remove_by_rank_ind){
 
-  if (is.null(sort_field) || dadmtools::is_blank(sort_field) || is.null(rank_type) || dadmtools::is_blank(rank_type) || (!rank_type %in% c('ASC','DESC'))   ) {
-    print(glue("ERROR: Please provide a sort_field and rank_type "))
+  if (is.null(rank_field) || dadmtools::is_blank(rank_field) || is.null(rank_type) || dadmtools::is_blank(rank_type) || (!rank_type %in% c('ASC','DESC'))   ) {
+    print(glue("ERROR: Please provide a rank_field and rank_type "))
     stop()
   } else{
-    if (!field_exists(attribute_table_no_schema,sort_field,attribute_table_schema)){
-      print(glue("ERROR: sort_field not in attribute table"))
+    if (!field_exists(attribute_table_no_schema,rank_field,attribute_table_schema)){
+      print(glue("ERROR: rank_field not in attribute table"))
       stop() }else{
 
 
-        dadmtools::run_sql_r(glue("DROP TABLE IF EXISTS {gr_skey_table}_no_duplicates_{rank_type}_{sort_field};"), pg_conn_param)
-        dadmtools::run_sql_r(glue("Create TABLE  {gr_skey_table}_no_duplicates_{rank_type}_{sort_field} as
+        dadmtools::run_sql_r(glue("DROP TABLE IF EXISTS {gr_skey_table}_no_duplicates_{rank_type}_{rank_field};"), pg_conn_param)
+        dadmtools::run_sql_r(glue("Create TABLE  {gr_skey_table}_no_duplicates_{rank_type}_{rank_field} as
 
                                   SELECT a.*
                                   FROM (SELECT grskey.gr_skey, att.*,
-                                  ROW_NUMBER() OVER (PARTITION BY grskey.gr_skey ORDER BY {sort_field} {rank_type}) ranked_order
+                                  ROW_NUMBER() OVER (PARTITION BY grskey.gr_skey ORDER BY {rank_field} {rank_type}) ranked_order
                                   FROM  {gr_skey_table} grskey join {attribute_table} att on
                                     grskey.pgid = att.pgid) a
                                   WHERE a.ranked_order = 1
                                     ;"), pg_conn_param)}
 
 
-    print(glue('Adding gr_skey as primary key to {gr_skey_table}_no_duplicates_{rank_type}_{sort_field}'))
-    dadmtools::run_sql_r(glue("ALTER TABLE {gr_skey_table}_no_duplicates_{rank_type}_{sort_field} ADD PRIMARY KEY (gr_skey);"), pg_conn_param)
+    print(glue('Adding gr_skey as primary key to {gr_skey_table}_no_duplicates_{rank_type}_{rank_field}'))
+    dadmtools::run_sql_r(glue("ALTER TABLE {gr_skey_table}_no_duplicates_{rank_type}_{rank_field} ADD PRIMARY KEY (gr_skey);"), pg_conn_param)
 
 
 
